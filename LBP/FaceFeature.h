7bf1097e76089d01b63afe13cf5ce868a73dd87b
liutongtongyeah <52691603+liutongtongyeah@users.noreply.github.com>
2021-08-11 12:42:38 +1200

    Update FaceFeature.h

--- LBP/FaceFeature.h
+++ LBP/FaceFeature.h
@@ -7,22 +7,8 @@
 using namespace std;
 using namespace cv;
 
-//extern int ex_blockCnt; // for Subspace_BlkFLD.cpp to use, seems unreachable
-/*
-faceSz is the same with the faceSz returned by InitAlign
-Feature length is returned for initializing feature vectors.
-If error occurs, 0 is returned.
-*/
 extern "C" __declspec(dllexport) int InitFeature(CvSize faceSz);
 
-
-/*
-ft32 is a column vector with type CV_FT_FC1
-*/
 extern "C" __declspec(dllexport) void GetFeature(CvArr *faceImg32, CvMat *ft);
 
-
-/*
-should be called before unloading the dll
-*/
 extern "C" __declspec(dllexport) void ReleaseFeature();
